372a3d3ae9c4b06cc2970a15d288569d5e8e6415
nicontrerasc8 <nicontrerasc8@gmail.com>
2024-01-18 18:00:14 -0500

    si

--- components/ubicacion.js
+++ components/ubicacion.js
@@ -10,9 +10,12 @@ const Ubicacion = () => {
   transition={{delay: .5}}
   className='section'>
     <h2 className='simple-h2'>Nuestra ubicación</h2>
-    <p>Jirón Inca 180, Surquillo, Lima.</p>
-    <p>Atendemos de lunes a domingo de 12am a 5pm.</p>
-    <a target={"_blank"} rel="noreferrer" href='https://ul.waze.com/ul?place=ChIJMx-EzHnJBZER55jBCjCk9Ao&ll=-12.11835170%2C-77.02039360&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location'>
+    <p>Calle Santa Rosa 131, Surquillo, Lima.</p>
+    <a target={"_blank"} rel="noreferrer" href='https://ul.waze.com/ul?place=ChIJF_b7dRDIBZERou1BcggIkIM&ll=-12.11872050%2C-77.02366760&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location'>
+    <button className='waze-button'><FaWaze/> Ir con Waze</button>
+    </a>
+    <p style={{marginTop: "calc(1rem + 10px)"}}>Calle Cabo Blanco Mzna B, Lote 3, Punta Hermosa</p>
+    <a target={"_blank"} rel="noreferrer" href='https://ul.waze.com/ul?place=EiZDYWJvIEJsYW5jbywgUHVudGEgSGVybW9zYSAxNTg0NiwgUGVydSIuKiwKFAoSCYNGl6-CogWREQnMP_xpvehfEhQKEgnH_qG5OpcFkRFm2KOV5RwLcQ&ll=-12.33416300%2C-76.82667970&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location'>
     <button className='waze-button'><FaWaze/> Ir con Waze</button>
     </a>
   </motion.div>
